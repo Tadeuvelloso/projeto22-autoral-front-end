15fb6d379375fdfc28ccd7882c73e26363cf61cf
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-06-18 19:20:06 -0300

    feat: colors constants

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+const baseColor = '#333333';
+const textBaseColor = '#FFFFFF';
+
+const detailColor = '#151515';
+const textAccentColor = '#B7B7B7';
+
+export default {
+    baseColor,
+    textBaseColor,
+    detailColor,
+    textAccentColor
+}
