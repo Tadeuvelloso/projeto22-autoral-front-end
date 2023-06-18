598ac74aaf0c62fcdbb5f50fdd63aa550656e0b1
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-06-18 19:19:27 -0300

    feat:  routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/signinPage';
+import GlobalStyle from './styles/GlobalStyle';
+import SignUp from './pages/signupPage';
 function App() {
-
-
   return (
-   <></>
+   <BrowserRouter>
+   <GlobalStyle />
+   <Routes>
+    <Route path='/' exact element={<SignIn/>}/>
+    <Route path='/signup' element={<SignUp />}/>
+    <Route path='/home'/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
